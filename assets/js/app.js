f0dde05bc64a4d6403dec5af3ca37324e489bf4f
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-19 14:27:00 +0200

    :sparkles: feat: js show addListmodal

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,21 @@ var app = {
   base_url: "http://localhost:3000/api",
 
   init: function () {
-    console.log("app.init !");
+    console.log(" hello, app.init !");
+    app.addListenerToActions();
 
   },
-  addList: function() {
-
+  addListenerToActions: function() {
+    const addListButton = document.getElementById("addListButton");
+    addListButton.addEventListener("click", app.showAddListModal);
   },
+  showAddListModal: function() {
+    const listModal = document.getElementById("addListModal");
+    listModal.classList.add("is-active");
+  }
+
 };
+
 
 // on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
 document.addEventListener("DOMContentLoaded", app.init);
